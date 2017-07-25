545109b91728e8f759a4e440a3932aac0d28ef0b
Dugas <David.Dugas@schwab.com>
2017-07-25 15:28:36 -0500

    routing updates

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -18,7 +18,7 @@ export class AppComponent implements OnInit {
   }
 
   ngOnInit() {
-    this.loadTeams();
+    // this.loadTeams();
   }
 
 
--- src/app/players/player-detail/player-detail.component.ts
+++ src/app/players/player-detail/player-detail.component.ts
@@ -5,7 +5,8 @@ import { NflPlayersService  } from '../../services/ffn/nfl-players.service';
 @Component({
   selector: 'app-player-detail',
   templateUrl: './player-detail.component.html',
-  styleUrls: ['./player-detail.component.css']
+  styleUrls: ['./player-detail.component.css'],
+  providers: [NflPlayersService]
 })
 export class PlayerDetailComponent implements OnInit {
 
